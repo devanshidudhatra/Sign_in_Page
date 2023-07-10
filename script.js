36041bd8b4a7a722104594c0fb8182a62cbf13b5
devanshidudhatra <devanshidudhatra77@gmail.com>
2023-07-10 18:06:58 +0530

    commit

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ username = document.getElementById('username')
 password = document.getElementById('password')
 cpassword = document.getElementById('cpassword')
 function myFunction(username,password,cpassword) {
-   document.getElementById('end').style.backgroundColor="black";
-   document.getElementById('cont').style.backgroundColor="black";
-   document.getElementById('cont').style.boxShadow="none";
-   document.getElementById('tab').style.background="black";
-   document.getElementById('tab').style.boxShadow="none";
-   document.getElementById('tab').style.border="none";
+   
+   // document.getElementById('cont').style.boxShadow="none";
+   // document.getElementById('tab').style.boxShadow="none";
+   // document.getElementById('tab').style.border="none";
    document.getElementById('tab').style.color="white";
-   document.getElementById('tab').innerHTML=`<h1>THANK YOU  ${username}</h1>`;
+   // document.getElementById('tab').innerHTML=`<h1>THANK YOU  ${username}</h1>`;
+
+   // Checking the length of Username
+   if(username.length > 16 || username.length < 5){
+      document.getElementById('tab').innerHTML= "Enter Proper Username";
+   }
+
 }
